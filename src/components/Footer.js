42ecc7111b73e64f3d35bb4859fd5e09b38620e4
Josh Ruff <missions.joshruff@gmail.com>
2019-03-21 22:20:34 -0500

    added simple social media link buttons to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { SocialIcon } from 'react-social-icons';
+
 
 
 const Footer = ({ contact }) => (
   <footer className='footer'>
+    <SocialIcon url="https://www.instagram.com/wrrstudio/" />
+    <SocialIcon url="https://www.facebook.com/wrrstudio/" />
     <h6 className='copyright'>
       {'© 2019 Wayne Ruff Studio. All right reserved.'}
     </h6>
